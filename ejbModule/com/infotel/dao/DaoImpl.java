f767b5e6ea13d991c9ad523397b5fd38aa98d2ae
IN-MP-028 <javajee2k19@gmail.com>
2019-05-15 14:18:58 +0200

    Commit interfaces + début DaoImpl

---
+++ ejbModule/com/infotel/dao/DaoImpl.java
@@ -0,0 +1,82 @@
+package com.infotel.dao;
+
+import java.util.List;
+
+import com.infotel.metier.Lotissement;
+import com.infotel.metier.Personne;
+
+public class DaoImpl implements IdaoLocal, IdaoRemote {
+
+	@Override
+	public void ajouterPersonne(Personne p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Personne> listerPersonnes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Personne getPersonne(long idPersonne) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void supprimerPersonne(long idPersonne) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void modifierPersonne(long idPersonne) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void ajouterLotissement(Lotissement l) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Lotissement> listerLotissements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Lotissement getLotissement(long idLot) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void supprimerLotissement(long idLot) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void modifierLotissement(long idLot) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void acheterLotissement(Personne p, Lotissement l) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void seMarier(Personne p1, Personne p2) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
