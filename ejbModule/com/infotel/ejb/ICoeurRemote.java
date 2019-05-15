9184aeb5663ffddede5d46717a86dba30e2fb7d2
Adam PROUST <adam.proust@outlook.fr>
2019-05-15 14:22:52 +0200

    maj EJB

---
+++ ejbModule/com/infotel/ejb/ICoeurRemote.java
@@ -0,0 +1,27 @@
+package com.infotel.ejb;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import com.infotel.metier.Lotissement;
+import com.infotel.metier.Personne;
+
+@Remote
+public interface ICoeurRemote {
+	
+	public void ajouterPersonne(Personne p);
+	public List<Personne> listerPersonnes();
+	public Personne getPersonne(long idPersonne);
+	public void supprimerPersonne(long idPersonne);
+	public void modifierPersonne(long idPersonne);
+	
+	public void ajouterLotissement(Lotissement l);
+	public List<Lotissement> listerLotissements();
+	public Lotissement getLotissement(long idLot);
+	public void supprimerLotissement(long idLot);
+	public void modifierLotissement(long idLot);
+	
+	public void acheterLotissement(Personne p, Lotissement l);
+	public void seMarier(Personne p1, Personne p2);
+}
