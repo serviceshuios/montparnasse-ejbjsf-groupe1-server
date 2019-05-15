8162a6f8800c9947ae5308b4609723e33f6fcd26
Adam PROUST <adam.proust@outlook.fr>
2019-05-15 15:04:37 +0200

    maj EJB

--- ejbModule/com/infotel/ejb/CoeurEJBImpl.java
+++ ejbModule/com/infotel/ejb/CoeurEJBImpl.java
@@ -38,85 +38,85 @@ public class CoeurEJBImpl implements ICoeurLocal, ICoeurRemote {
 	@Override
 	public void ajouterPersonne(Personne p) {
 		// TODO Auto-generated method stub
-		
+		daoR.ajouterPersonne(p);
 	}
 
 	@Override
 	public List<Personne> listerPersonnes() {
 		// TODO Auto-generated method stub
-		return null;
+		return daoR.listerPersonnes();
 	}
 
 	@Override
 	public Personne getPersonne(long idPersonne) {
 		// TODO Auto-generated method stub
-		return null;
+		return daoR.getPersonne(idPersonne);
 	}
 
 	@Override
 	public Personne getPersonneSuppr(long idPersonne) {
 		// TODO Auto-generated method stub
-		return null;
+		return daoR.getPersonneSuppr(idPersonne);
 	}
 
 	@Override
 	public void supprimerPersonne(Personne p) {
 		// TODO Auto-generated method stub
-		
+		daoR.supprimerPersonne(p);
 	}
 
 	@Override
 	public void modifierPersonne(Personne p) {
 		// TODO Auto-generated method stub
-		
+		daoR.modifierPersonne(p);
 	}
 
 	@Override
 	public void ajouterLotissement(Lotissement l) {
 		// TODO Auto-generated method stub
-		
+		daoR.ajouterLotissement(l);
 	}
 
 	@Override
 	public List<Lotissement> listerLotissements() {
 		// TODO Auto-generated method stub
-		return null;
+		return daoR.listerLotissements();
 	}
 
 	@Override
 	public Lotissement getLotissement(long idLot) {
 		// TODO Auto-generated method stub
-		return null;
+		return daoR.getLotissement(idLot);
 	}
 
 	@Override
 	public Lotissement getLotissementSuppr(long idLot) {
 		// TODO Auto-generated method stub
-		return null;
+		return daoR.getLotissementSuppr(idLot);
 	}
 
 	@Override
 	public void supprimerLotissement(Lotissement l) {
 		// TODO Auto-generated method stub
-		
+		daoR.supprimerLotissement(l);
 	}
 
 	@Override
 	public void modifierLotissement(Lotissement l) {
 		// TODO Auto-generated method stub
-		
+		daoR.modifierLotissement(l);
 	}
 
 	@Override
 	public void acheterLotissement(Personne p, Lotissement l) {
 		// TODO Auto-generated method stub
-		
+		daoR.acheterLotissement(p, l);
 	}
 
 	@Override
 	public void seMarier(Personne p1, Personne p2) {
 		// TODO Auto-generated method stub
-		
+		daoR.seMarier(p1, p2);
 	}
 
 	
